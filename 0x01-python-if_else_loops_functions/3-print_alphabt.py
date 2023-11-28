43c5b2d59f2a5fedeb8c8cb83ac747c51f4d9a71
Ompheletse Makwela <omphentse@gmail.com>
2023-11-28 15:46:25 -0800

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-alphabet = ''.join(chr(i) for i in range(97, 123))
+alphabet = ''.join(chr(i) for i in range(97, 123) if chr(i) not in ['q', 'e'])
 output_string = "{}".format(alphabet)
 
 print(output_string, end='')
