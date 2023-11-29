78fbd28de61637f20600d8c2928195404d73a58a
Ompheletse Makwela <omphentse@gmail.com>
2023-11-28 16:09:22 -0800

    task6 and task7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def islower(c):
+    ascii_value = ord(c)
+    return True if 97 <= ascii_value <= 122 else False
+
+if __name__ == "__main__":
+    print("a is {}".format("lower" if islower("a") else "upper"))
+    print("H is {}".format("lower" if islower("H") else "upper"))
+    print("A is {}".format("lower" if islower("A") else "upper"))
+    print("3 is {}".format("lower" if islower("3") else "upper"))
+    print("g is {}".format("lower" if islower("g") else "upper"))
