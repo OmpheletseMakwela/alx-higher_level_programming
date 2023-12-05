8862daf62e4110e1cd021ee086cbc996b5aee9a2
Ompheletse Makwela <omphentse@gmail.com>
2023-12-05 11:08:24 +0200

    task5 up

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    char_to_remove = 'cC'
-    for char in char_to_remove:
-        my_string = my_string.replace('c', '')
-        my_string = my_string.replace('C', '')
-        return my_string
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
