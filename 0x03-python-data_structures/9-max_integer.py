8faee21ba4620dd884c6603954ad21c127e76d2b
Ompheletse Makwela <omphentse@gmail.com>
2023-12-05 11:14:55 +0200

    task9 update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,11 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-        max_val = my_list[0]
-        for num in my_list:
-            if num > max_val:
-                return max_val
 
+    max_value = my_list[0]
+
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
