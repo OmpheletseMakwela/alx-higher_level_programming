b6bd2770345be03a7937e3c0fdbef30a86e2c570
Ompheletse Makwela <omphentse@gmail.com>
2023-12-06 09:16:43 +0200

    task0 update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-import add_0
 
 a = 1
 b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+from add_0 import add
+
+result = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, result))
