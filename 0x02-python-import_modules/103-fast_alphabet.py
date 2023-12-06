891d02d24fc79b3d45d0ddcc36cffd184e714ebc
Ompheletse Makwela <omphentse@gmail.com>
2023-12-06 15:02:30 +0200

    task9 update

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
 import sys
-
-alphabet = getattr(__import__("string"), "ascii_uppercase") + "\n"
-getattr(sys.stdout, "write")(alphabet)
+getattr(sys.stdout, "write")(getattr(__import__("string"), "ascii_uppercase") + "\n")
