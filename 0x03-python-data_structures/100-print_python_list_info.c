e4726d86cc2c3a557f5201dfc10e181ed234dbc9
Ompheletse Makwela <omphentse@gmail.com>
2023-12-06 16:13:48 +0200

    task4

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,18 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+#include "lists.h"
 #include <stdio.h>
 
-void print_python_list_info(PyObject *p)
-{
-    Py_ssize_t size, allocated, i;
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t size, alloc, i;
     PyObject *item;
 
     size = PyList_Size(p);
-    allocated = ((PyListObject *)p)->allocated;
+    alloc = ((PyListObject *)p)->allocated;
 
     printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", allocated);
+    printf("[*] Allocated = %ld\n", alloc);
 
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++) {
         item = PyList_GetItem(p, i);
         printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
-
