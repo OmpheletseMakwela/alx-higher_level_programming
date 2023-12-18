afe991cc161fc6554224764f9521af9f3df9672c
Ompheletse Makwela <omphentse@gmail.com>
2023-12-18 19:30:44 +0200

    task4

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     element = 0
     try:
@@ -10,5 +9,4 @@ def safe_print_list(my_list=[], x=0):
         pass
     finally:
         print()    
-       
         return element
