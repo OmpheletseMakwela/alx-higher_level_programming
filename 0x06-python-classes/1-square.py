2958cc029e88858bbbd01534541e18a32d724ada
Ompheletse Makwela <omphentse@gmail.com>
2023-12-19 15:14:34 +0200

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """This class represents a square."""
+    def __init__(self, size):
+        """Initialize the sqaure class with a given size."""
+        self.__size = size
