da73da267c331813581b28ee772f2a35927d19b7
Ompheletse Makwela <omphentse@gmail.com>
2024-01-09 21:08:00 +0200

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""A function is defined"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+    - my_obj: The object to be converted to a JSON string.
+
+    Returns:
+    - str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
