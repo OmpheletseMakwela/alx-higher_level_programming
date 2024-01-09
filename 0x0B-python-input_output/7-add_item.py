06594c8bca85fe62f1e24491c91863f9283c8d3b
Ompheletse Makwela <omphentse@gmail.com>
2024-01-09 22:21:34 +0200

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,62 @@
 #!/usr/bin/python3
 """import a module"""
+import json
 import sys
 from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from typing import List
+"""A function is defined."""
 
-args = sys.argv[1:]  # Get all command-line arguments except the script name
 
-filename = "add_item.json"
+def save_to_json_file(my_obj: List, filename: str):
+    """
+    Save a Python object to a file in JSON format.
 
-# If the file doesn't exist, create an empty list
-if not path.exists(filename):
-    save_to_json_file([], filename)
+    Args:
+        my_obj (List): Python object to be saved.
+        filename (str): Name of the file to save the object.
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
 
-# Load the existing list from the file
-items_list = load_from_json_file(filename)
+def load_from_json_file(filename: str) -> List:
+    """
+    Load a Python object from a file in JSON format.
 
-# Add new arguments to the existing list
-items_list.extend(args)
+    Args:
+        filename (str): Name of the file to load the object from.
 
-# Save the updated list to the file
-save_to_json_file(items_list, filename)
+    Returns:
+        List: Loaded Python object.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+def add_arguments_to_list(arguments: List[str], filename: str):
+    """
+    Add all arguments to a Python list and save them to a file in JSON format.
+
+    Args:
+        arguments (List[str]): List of arguments to add.
+        filename (str): Name of the file to save the list.
+    """
+    if path.exists(filename):
+        # Load the existing list from the file
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
+    # Add new arguments to the list
+    my_list.extend(arguments)
+
+    # Save the updated list to the file
+    save_to_json_file(my_list, filename)
+
+if __name__ == "__main__":
+    # Extract command-line arguments excluding the script name
+    arguments = sys.argv[1:]
+    
+    # Name of the file to save the list
+    filename = 'add_item.json'
+
+    # Add arguments to the list and save it to a file
+    add_arguments_to_list(arguments, filename)
