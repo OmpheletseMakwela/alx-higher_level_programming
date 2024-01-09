be38774770ac6c396103c276dd8361b9a46d178c
Ompheletse Makwela <omphentse@gmail.com>
2024-01-09 21:54:33 +0200

    task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""A class is created."""
+
+
+class Student:
+    """
+    Defines a student with first_name, last_name, and age attributes.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+        - first_name (str): The first name of the student.
+        - last_name (str): The last name of the student.
+        - age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+        - attrs (list): List of strings specifying the attributes.
+
+        Returns:
+        - dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        filtered_attributes = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_attributes[attr] = getattr(self, attr)
+        return filtered_attributes
